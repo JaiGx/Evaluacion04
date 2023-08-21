8ef5ade06662433e0d7fc6d9acd8ae303b06f249
JaiGx <134289866+JaiGx@users.noreply.github.com>
2023-08-21 12:23:07 +0000

    hola

---
+++ evaluacion04/src/main/java/com/example/evaluacion04/Evaluacion04Application.java
@@ -0,0 +1,13 @@
+package com.example.evaluacion04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Evaluacion04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Evaluacion04Application.class, args);
+	}
+
+}
